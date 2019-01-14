8d1d880d2a41629c6a3605c33081ecfbb0e9ff4b
Sai Maduri <10012826@sbschools.org>
2019-01-14 08:45:59 -0500

    Updated ThreadDemo2

--- ThreadDemo2.java
+++ ThreadDemo2.java
@@ -1,7 +1,34 @@
 public class ThreadDemo2 {
 
-	public static void main (String[]args) {
-	
+	static int i = 0;
+	static int si = 0;
+
+	public static synchronized void addMethod() {
+		si++;
 	}
-	
+
+	public static void main (String[]args) {
+
+		for (int j = 0; j < 200; j++ )
+			new MyThread().start();
+		try {
+			Thread.sleep(5000);
+		} catch(Exception e) {}
+		System.out.println("value of i: " + i);
+				System.out.println("value of si: " + si);
+
+	}
+
+	public static class MyThread extends Thread {
+
+		public void run() {
+			try {
+				Thread.sleep(2);
+			} catch(Exception e) {}
+			i++;
+			addMethod();
+		}
+
+	}
+
 }
